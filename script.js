fe665ac38c83b09520a1304d4ebe5eb5a0683be3
parthawate-2793 <119276800+parthawate-2793@users.noreply.github.com>
2024-02-06 22:54:49 +0530

    Commit#2 to changebg

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const button = document.getElementById("btn");
 const body = document.body;
-const colors=['red','blue','green','yellow','violet'];
+const colors=['red','blue','green','yellow','violet','indigo'];
 body.style.backgroundColor=colors[0];
 button.addEventListener('click',changeBackground);
 function changeBackground(){
